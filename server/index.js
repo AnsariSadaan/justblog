e7053ede45f04a5ee88957ffe2e5204fbb0f83b7
sadaan <ansarisadaan72@gmail.com>
2023-08-18 16:50:05 +0530

    added image uplaod functionality and set path for endpoints

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const dotenv = require('dotenv')
 
+dotenv.config();
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/server/auth", authRoute);
+app.use("/server/users", usersRoute);
+app.use("/server/posts", postRoute);
+app.use("/server/categories", categoryRoute);
 
-mongoose.connect('mongodb+srv://justblog:justblog@justblog.irnphgb.mongodb.net/')
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Database Connected Successfully"))
 .catch((err)=>{console.log(err)})
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, "hello.jpeg");
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/server/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port no : ${PORT}`)
