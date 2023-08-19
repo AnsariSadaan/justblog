8207e618c32f544b0d19dec8101e7adc127ff7a4
sadaan <ansarisadaan72@gmail.com>
2023-08-19 20:47:40 +0530

    proxy change

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+  
+// })
+
 export default defineConfig({
+  // ... other config properties
+  server: {
+    proxy: {
+      '/server': 'http://localhost:3000'
+    }
+  },
   plugins: [react()],
-})
+});
